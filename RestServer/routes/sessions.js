19600367a4cc1e4e4f02dcec58ac9acf8e2bad6e
Przemysław Gołębski <emreis@vindicator.home>
2019-12-03 23:25:10 +0100

    sessions

--- RestServer/routes/sessions.js
+++ RestServer/routes/sessions.js
@@ -4,11 +4,12 @@ var router = express.Router();
 let idCounter = 1;
 
 const sessions = [
-    {id:idCounter++, fname:'Filip', lname:'Jatelnicki',bonus:3,contract:'service' },
-    {id:idCounter++, fname:'Jakub', lname:'Dzieciatko',bonus:4,contract:'normal' },
-    {id:idCounter++, fname:'Pawel', lname:'Kalbarczyk',bonus:5,contract:'B2B' },
-    {id:idCounter++, fname:'Wojciech', lname:'Szadurski',bonus:0,contract:'trash' },
-    {id:idCounter++, fname:'Wiktor', lname:'Androsiuk',bonus:6,contract:'service' }
+    {id:idCounter++, clientid:1, empid:1, start:'12:30', end:'13:30',station:3},
+    {id:idCounter++, clientid:1, empid:2, start:'13:30', end:'13:30',station:2},
+    {id:idCounter++, clientid:1, empid:1, start:'12:30', end:'13:30',station:1},
+    {id:idCounter++, clientid:2, empid:4, start:'09:30', end:'10:30',station:3},
+    {id:idCounter++, clientid:3, empid:3, start:'12:30', end:'13:30',station:5},
+    {id:idCounter++, clientid:4, empid:1, start:'12:30', end:'13:30',station:3},
 ];
 
 router.get('/getAll', (req, res, next) => {
@@ -20,36 +21,37 @@ router.get('/getById', (req, res, next) => {
     if(qid == null) {
         res.status(404).send('Please provide valid id');
     } else {
-        const emp = emps.find(e => e.id = qid);
-        if(emp === null) {
-            res.send(404).send('No emp find with given id');
+        const session = sessions.find(s => s.id = qid);
+        if(session === null) {
+            res.send(404).send('No session find with given id');
         } else {
-            res.send(emp);
+            res.send(session);
         }
     }
 });
 
 router.put('/create', (req, res, next) => {
-    const fname = req.headers.fname;
-    const lname = req.headers.lname;
-    const bonus = req.headers.bonus;
-    const contractType = req.headers.contract;
+    const empId = req.headers.empid;
+    const clientId = req.headers.clientid;
+    const start = req.headers.start;
+    const end = req.headers.end;
+    const station = req.headers.station;
 
-    emps.push({id:idCounter++,fname:fname,lname:lname,bonus:bonus,contract:contractType});
-    res.status(201).send('Create a new employee');
+    sessions.push({id:idCounter++,clientid:clientId,empid:empId,start:start,end:end,station:station});
+    res.status(201).send('Create a new session');
 });
 
 router.delete('/deleteById', (req, res, next) => {
     if(req.query.id == null) {
-      res.send('Plsease provide valid id');
+      res.send('Please provide valid id');
     } else {
-      const emp = emps.find(u => u.id === parseInt(req.query.id));
-      if(!emp) {
-        res.status(404).send('No emp with this id found');
+      const session = sessions.find(u => u.id === parseInt(req.query.id));
+      if(!session) {
+        res.status(404).send('No session with this id found');
       } else {
-        const index = emps.indexOf(emp);
-        emps.splice(index, 1);
-        res.send('emp deleted succesfully');
+        const index = sessions.indexOf(session);
+        sessions.splice(index, 1);
+        res.send('session deleted succesfully');
       }
     }
   });
@@ -58,18 +60,19 @@ router.patch('/updateById', (req, res, next) => {
     if(req.query.id == null) {
         res.send('Plsease provide valid id');
       } else {
-        const emp = emps.find(u => u.id === parseInt(req.query.id));
-        if(!emp) {
-          res.status(404).send('No emp with this id found');
+        const session = sessions.find(u => u.id === parseInt(req.query.id));
+        if(!session) {
+          res.status(404).send('No session with this id found');
         } else {
-            const index = emps.indexOf(emp);
-            const fname = req.headers.fname;
-            const lname = req.headers.lname;
-            const bonus = req.headers.bonus;
-            const contractType = req.headers.contract;
-            const updatedEmp = {id:emps[index].id, fname:fname, lname:lname, bonus:bonus, contract:contractType};
-            emps[index] = updatedEmp;
-            res.send('emp updated succesfully');
+            const index = sessions.indexOf(session);
+            const clientId = req.headers.clientid;
+            const empId = req.headers.empid;
+            const start = req.headers.start;
+            const end = req.headers.end;
+            const station = req.headers.station;
+            const updatedSession = {id:sessions[index].id, clientid:clientId, empid:empId, start:start, end:end,station:station};
+            sessions[index] = updatedSession;
+            res.send('session updated succesfully');
         }
       }
 });
