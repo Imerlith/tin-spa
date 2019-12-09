5c3ed6ef7058dca0e4dd7621b787a528e0568a3c
Przemysław Gołębski <przemyslawgolebski@gmail.com>
2019-12-09 09:19:39 +0100

    react from scratches

--- spa/src/App.js
+++ spa/src/App.js
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import TopPanel from './topPanel';
+import './main_style.css';
+import BottomPanel from './bottomPanel';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class AppComponent extends React.Component {
+
+    render(){
+        return (
+            <div className='app'>
+                <TopPanel />
+                <BottomPanel />
+            </div>
+        );
+    };
 }
 
-export default App;
+export default AppComponent;
---
+++ spa/src/bottomPanel.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+class BottomPanelComponent extends React.Component {
+    render() {
+        return(
+            <footer>
+                <p>Created by sdadasdad</p>
+            </footer>
+        );
+    };
+
+}
+
+export default BottomPanelComponent;
