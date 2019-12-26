23daf49455c4b0e6724c0ded643f6dbc0fd292fd
Przemysław Gołębski <przemyslawgolebski@gmail.com>
2019-12-26 01:04:12 +0100

    hamburger menu

--- spa/src/topPanel.js
+++ spa/src/topPanel.js
@@ -12,6 +12,7 @@ class TopPanelComponent extends React.Component {
             <div className='top-panel'>
                 <h1>XxX_G4m3r$_D3nn_XxX</h1>
                 <NavbarComponent isLoggedIn = {this.state.isLoggedIn}/>
+                <HamburgerMenuComponent isLoggedIn = {this.state.isLoggedIn} />
             </div>
         );
     };
@@ -36,6 +37,39 @@ class NavbarComponent extends React.Component {
                     <li>About us</li>
                     <li>{logButton}</li>
                 </ul>
+            </div>
+        );
+    };
+}
+
+
+class HamburgerMenuComponent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLoggedIn: props.isLoggedIn,
+        }
+    }
+
+    render() {
+        const isLoggedIn = this.state.isLoggedIn;
+        const logButton = isLoggedIn ? 'Logout' : 'Login';
+        return(
+            <div className="menu-wrap">
+            <input type="checkbox" className="toggler" />
+                <div className="hamburger"><div></div></div>
+                    <div className="menu">
+                        <div>
+                            <div>
+                                <ul>
+                                    <li>Prices</li>
+                                    <li>Avaliable Stations</li>
+                                    <li>About us</li>
+                                    <li>{logButton}</li>
+                                </ul>
+                            </div>
+                        </div>
+                </div>
             </div>
         );
     };
