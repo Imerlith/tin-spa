f51485b1b547597e6f1af35e8c23e143c8d52766
Przemysław Gołębski <przemyslawgolebski@gmail.com>
2020-01-15 23:43:15 +0100

    modify client without styles

--- spa/src/modifyClient.js
+++ spa/src/modifyClient.js
@@ -3,14 +3,42 @@ import React from 'react';
 class ModifyClientComponent extends React.Component {
     constructor(props) {
         super(props);
+        if(this.props.toUpdate === null) {
+            this.state = {
+                client: {
+                    Client_Id: '',
+                    First_Name: '',
+                    Last_Name: '',
+                    Last_Visit_Date: '',
+                    Birthday: '',
+                    Favourite_Game: ''
+                }
+            }
+        }
+        else {
+            this.state = {client: this.props.toUpdate};
+        }
     }
 
 
 
     render() {
         return (
-            <div className="client-records-container">
-               
+            <div className="modify-client-container">
+                <form id="client-form">
+                    <label htmlFor='cFirstName'>First Name</label>
+                    <label htmlFor='cLastName' > Last Name</label>
+                    <label htmlFor='cLV'>Last Visit</label>
+                    <label htmlFor='cBirthday'>Birthday</label>
+                    <label htmlFor='cFG'>Favourite_Game</label>
+                    <input id='cFirstName' defaultValue={this.state.client.First_Name}  type='text'/>
+                    <input id='cLastName' defaultValue={this.state.client.Last_Name} type="text"/>
+                    <input id='cLV' defaultValue={this.state.client.Last_Visit_Date} type="date"/>
+                    <input id='cBirthday' defaultValue={this.state.client.Birthday} type="date"/>
+                    <input id='cFG' defaultValue={this.state.client.Favourite_Game} type="text"/>
+                    <button>Accept</button>
+                    <button>Reject</button>
+                </form>
             </div>
         );
     }
